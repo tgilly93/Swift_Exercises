b00f0f5f2ae3cc236b48979d98e3f446fefdf0ae
Terry Gilmore <tgilly93@gmail.com>
2022-10-03 10:39:51 -0400

    Initial Commit

---
+++ Const_Var.playground/Contents.swift
@@ -0,0 +1,12 @@
+let day = "Monday"
+let dailyTemperature = 75
+print("Today is \(day). Rise and shine!")
+print("The temperature on \(day) is \(dailyTemperature) F")
+
+var temperature = 70
+temperature = 80
+print("The temperature on \(day) morning is \(temperature) F")
+
+let weeklyTemperature = 75
+temperature = weeklyTemperature
+print("The average temperature this week is \(temperature) F")
