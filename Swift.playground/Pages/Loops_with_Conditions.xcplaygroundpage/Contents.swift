1c01aa6956b14169a44167dd735f9d345789a113
Terry Gilmore <tgilly93@gmail.com>
2022-10-03 16:17:13 -0400

    Loops with Conditional exercise in Swift.

---
+++ Swift.playground/Pages/Loops_with_Conditions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+let levels = 10
+let freeLevels = 4
+let bonusLevel = 3
+
+for level in 1...levels {
+    if level == bonusLevel {
+        print("Skip bonus level \(bonusLevel).")
+        continue
+    }
+    print("Play level \(level).")
+    if level == freeLevels {
+        print("You have played all \(freeLevels) free levels. Purchase game to play the remaining \(levels - freeLevels) levels.")
+        break
+    }
+}
