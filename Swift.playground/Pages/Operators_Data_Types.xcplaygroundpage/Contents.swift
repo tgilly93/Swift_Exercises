baa3111b24e4f4587fd58d1231c10ce8c3209721
Terry Gilmore <tgilly93@gmail.com>
2022-10-03 11:54:05 -0400

    operators and Data Types with Swift programming.

--- Swift.playground/Pages/Operators_Data_Types.xcplaygroundpage/Contents.swift
+++ Swift.playground/Pages/Operators_Data_Types.xcplaygroundpage/Contents.swift
@@ -4,6 +4,7 @@ gameScore += levelScore
 print("The game's score is \(gameScore)")
 var levelBonusScore = 10.0
 levelBonusScore = 20
+levelBonusScore = 40
 print("The level's bonus score is \(levelBonusScore).")
 gameScore += Int(levelBonusScore)
 print("The game's final score is \(gameScore).")
