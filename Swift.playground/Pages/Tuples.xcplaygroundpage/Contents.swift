2abaa491e5ca240ffc68bd92efbef01aaee5c36c
Terry Gilmore <tgilly93@gmail.com>
2022-10-04 11:50:44 -0400

    Tuple exercises with Swift programming.

---
+++ Swift.playground/Pages/Tuples.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+let credentials = ("", -1111)
+if credentials.0 == "" || credentials.1 < 0 {
+    print("Invalid credentials!")
+}
+else {
+    print("The username is \(credentials.0) and the passcode is \(credentials.1).")
+}
+let fullCredentials = (password : "pass", passcode : 1111)
+if fullCredentials.password == "" || fullCredentials.passcode < 0 {
+    print("Invalid credentials!")
+}
+else {
+    print("The username is \(fullCredentials.password) and the passcode is \(fullCredentials.passcode).")
+}
